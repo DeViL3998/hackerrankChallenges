4dd58d7882f9d580e16ef4d99b286288aac3d11d
DeViL3998 <adarsh.brata@gmail.com>
2019-01-13 04:31:22 +0530

    solved one challenge

---
+++ coin exchange challenge(recursion).py
@@ -0,0 +1,19 @@
+def nWays(money,coinCurr,coins):
+    if money==0:
+        return 1
+    if money<0:
+        return 0
+
+    ways = 0
+
+    for coin in range(coinCurr, len(coins)):
+        ways += nWays(money-coins[coin], coin, coins)
+
+    return ways
+
+
+mn = list(map(int,input().split()))
+money = mn[0]
+coins= list(map(int, input().split()))
+
+print(nWays(money,0,coins))
